c0d1c142ae7316989aec4bf212cdc3e599307fdc
Lucas Michelini <lucasmro@gmail.com>
2013-07-09 21:10:55 +0000

    Revised grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var DOWNLOADED_FILE_DEFAULT = "downloaded.file";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +64,34 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+function executeCheck (file, checks) {
+    var checkJson = checkHtmlFile(file, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+   
+    var file = program.file;
+
+    if (program.url) {
+      rest.get(program.url)
+      .on('complete', function(response) {
+          fs.writeFileSync(DOWNLOADED_FILE_DEFAULT, response);
+          executeCheck(DOWNLOADED_FILE_DEFAULT, program.checks);
+      })
+      .on('error', function(err) { 
+          console.log('An error occurred:' + err); 
+          process.exit(1);
+      });
+    } else {
+      executeCheck(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
